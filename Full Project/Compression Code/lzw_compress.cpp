18374b3881ca72b8395f87ae807f4054c451334c
MChiragV <122266494+MChiragV@users.noreply.github.com>
2024-11-08 21:25:50 +0530

    Update lzw_compress.cpp

--- Full Project/Compression Code/lzw_compress.cpp
+++ Full Project/Compression Code/lzw_compress.cpp
@@ -12,8 +12,8 @@ void init_dictionary(short dictionary[MAX_DICT_SIZE][MAX_SEQ_LEN], short &dict_s
 
 // Find the longest match in the dictionary
 short find_in_dictionary(short dictionary[MAX_DICT_SIZE][MAX_SEQ_LEN], short dict_size, short *seq, short seq_len) {
-    # pragma HLS unroll
     find_in_dictionary_label0:for (short i = 0; i < dict_size; i++) {
+	# pragma HLS unroll
         bool match = true;
         find_in_dictionary_label3:for (short j = 0; j < seq_len; j++) {
             if (dictionary[i][j] != seq[j]) {
